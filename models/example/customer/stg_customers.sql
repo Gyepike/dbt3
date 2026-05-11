d14f045d55e39993f1f90851897eb7c70e576895
Péter Attila Antal <antipet@gmail.com>
2026-05-11 13:21:41 +0000

    updated 20260511

--- models/example/customer/stg_customers.sql
+++ models/example/customer/stg_customers.sql
@@ -1,10 +1,12 @@
-WITH stg_customers AS (
-    SELECT
-        customer_id,
-        first_name||' '||last_name as customer_name,
-        email as email_address,
-        address as billing_address
-    FROM {{ source("public","customer")}}
-)
+with
+    stg_customers as (
+        select
+            customer_id,
+            first_name || ' ' || last_name as customer_name,
+            email as email_address,
+            address as billing_address
+        from {{ source("public", "customer") }}
+    )
 
-SELECT * FROM stg_customers
+select *
+from stg_customers
